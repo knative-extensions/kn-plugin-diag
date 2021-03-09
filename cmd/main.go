6f2904cc9dcce98cb072867020625af9ecca98f3
Liu Ying <cdlliuy@cn.ibm.com>
2021-03-09 17:30:55 +0800

    add initial code

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"knative.dev/kn-plugin-diag/pkg/command/diagnose"
+	// "strings"
+	// "time"
 
 	"github.com/spf13/cobra"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	"knative.dev/kn-plugin-diag/pkg/command/diagnose"
+	"knative.dev/kn-plugin-diag/pkg/utils"
 )
 
 func main() {
 
-	//p := &utils.ConnectionConfig{}
-	//p.Initialize()
+	p := &utils.ConnectionConfig{}
+	p.Initialize()
 
 	rootCmd := &cobra.Command{
-		Use:   "kn-diag",
+		Use:   "knative-diagnose",
 		Short: "A CLI of to help with Diagnose Knative Resources",
 		Long:  `A CLI of to help with Diagnose Knative Resources.`,
 	}
-	rootCmd.AddCommand(diagnose.NewServiceCmd())
+	rootCmd.AddCommand(diagnose.NewServiceCmd(p))
 	rootCmd.InitDefaultHelpCmd()
 
 	if err := rootCmd.Execute(); err != nil {
